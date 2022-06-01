cb4be7d16f113132872b5049f981f9ca6bc83e06
benjamin-lai <benjamin.lai@hotmail.com>
2022-06-01 15:11:09 +1000

    fifth is sum of doubles

---
+++ src/practiseBen/Fifth.java
@@ -0,0 +1,13 @@
+package practiseBen;
+
+public class Fifth {
+    public static void main(String[] args) {
+        double x = Double.parseDouble(args[0]);
+        double y =  Double.parseDouble(args[1]);
+        double z = Double.parseDouble(args[2]);
+
+        double sum = x + y + z;
+
+        System.out.println((sum));
+    }
+}
