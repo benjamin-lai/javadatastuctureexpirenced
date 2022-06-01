262a91aa82942925ab8ecc4591a8f5a418db2c0e
benjamin-lai <benjamin.lai@hotmail.com>
2022-06-01 15:05:48 +1000

    Changed fourth to take in args

--- src/practiseBen/Fourth.java
+++ src/practiseBen/Fourth.java
@@ -2,9 +2,9 @@ package practiseBen;
 
 public class Fourth {
     public static void main(String[] args) {
-        int x = 1;
-        int y =  2;
-        int z = 3;
+        int x = Integer.parseInt(args[0]);
+        int y =  Integer.parseInt(args[1]);
+        int z = Integer.parseInt(args[2]);
 
         int sum = x + y + z;
 
